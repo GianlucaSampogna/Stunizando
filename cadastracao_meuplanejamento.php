515a836ab201aef14fa9c8529a4084fd598c51a7
Gianluca Scalzi Sampogna <84414809+GianlucaSampogna@users.noreply.github.com>
2022-09-30 10:11:36 -0300

    Add files via upload

--- cadastracao_meuplanejamento.php
+++ cadastracao_meuplanejamento.php
@@ -45,10 +45,6 @@ if (isset($_POST["biologia"])) {
     $biologia = 0;
 }
 
-
-
-
-
 //pega os dados no cadastro e os transforma em variáveis
 
 $id_usuario = $_POST['id_usuario'];
@@ -78,12 +74,12 @@ $ultimoID = pg_fetch_array($result,0)[0];
 
 $i = 0;
 $num = 1;
-$info = array($hrs_dom, $hrs_sab, $hrs_sex, $hrs_qui, $hrs_qua, $hrs_ter, $hrs_seg);
+$info = array($hrs_seg, $hrs_ter, $hrs_qua, $hrs_qui, $hrs_sex, $hrs_sab, $hrs_dom);
 list($a[0], $a[1], $a[2], $a[3], $a[4], $a[5], $a[6],) = $info; 
 
 for ($i = 0; $i <=6; $i++){
-    $sql = "INSERT INTO plan_dia (fk_planejamento_id, fk_dia_semana_id, qtd_horas) 
-    VALUES('$ultimoID','$num', '$info[$i]') ";
+    $sql = "INSERT INTO plan_dia ( fk_dia_semana_id, qtd_horas) 
+    VALUES('$num', '$info[$i]') ";
     $result = pg_query($conexao, $sql);
     $num = $num + 1;
 }
