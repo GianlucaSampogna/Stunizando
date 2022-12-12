300e305cc869fc20ce5e088ed519855fed6eea9c
Gianluca Scalzi Sampogna <84414809+GianlucaSampogna@users.noreply.github.com>
2022-12-12 17:51:36 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ include_once 'header.php'; ///Incluição do header
         <section>
             <!--Primeiro texto-->
 		    <div class="row margin1 mb-1">
-                <div class="col-sm-4 offset-sm-1">
+                <div class="col-sm-3 offset-sm-2">
 				    <h1>Feito para facilitar o seu estudo e a sua rotina!</h1>
-				    <p class="mt-4">O estunizado vai te auxiliar na criação de um cronograma que se adeque à sua realidade e torne o estudo prático e prazeroso.<p>
+				    <p class="mt-4">O stunizado vai te auxiliar na criação de um cronograma que se adeque à sua realidade e torne o estudo prático e prazeroso.<p>
 			    </div>
 			        <!--Primeira imagem-->
 			        <div class="col-sm-5 offset-sm-1">
@@ -22,11 +22,11 @@ include_once 'header.php'; ///Incluição do header
 
 		    <!--Segunda imagem-->
 			<div class="row margin1 mb-1">
-			    <div class="col-auto offset-sm-1">
+			    <div class="col-auto offset-sm-2">
 				    <img  class="imagem4" src="img/imagem4.jpeg" alt="1" title="início" />
 			    </div>
                 <!--Segundo texto-->
-			    <div class="col-sm-6 offset-sm-1">
+			    <div class="col-sm-4 offset-sm-1">
 				    <h1>O que é o Stunizado?</h1>
 					<p class="mt-4">O stunizado foi criado por alunos para alunos, pensando nas próprias necessidades os desenvolvedores idealizaram esse programa com o intuido de atender as suas necessidades também. Aqui você vai encontrar o auxílio que precisa para saber o que estudar e quando estudar de maneira simplificada, melhorando e facilitando o seu desenvolvimento para a prova do ENEM. Vem com a gente? <p>
 			    </div>
@@ -34,7 +34,7 @@ include_once 'header.php'; ///Incluição do header
 
 			<!--Terceiro texto-->
 		    <div class="row margin1 mb-1">
-			    <div class="col-sm-6 offset-md-1">
+			    <div class="col-sm-4 offset-md-2">
 				    <h1>Porque ele pode ser útil para você?</h1>
 				    <p class="mt-4">Agora você não vai mais precisar se preocupar com qual matéria estudar e quando, aqui você vai poder encontrar todas essas informações e agilizar o seu processo. O stunizado tem a proposta de melhorar a sua caminhada e para isso reduzir as dores de cabeça que você pode encontrar nela.<p>
 			    <!--Terceira imagem-->
@@ -47,7 +47,7 @@ include_once 'header.php'; ///Incluição do header
         <!--Início container ferramentas-->
 		    <div class="container">
 		    <div class="row mb-5">
-			    <h1 class="col-sm-10 text-center">Ferramentas</h1>
+			    <h1 class="col-sm-12 text-center">Ferramentas</h1>
 
 				    <div class="col-sm-4 mt-4 m-0">
 				    <div class="card p-0 ">
@@ -73,8 +73,8 @@ include_once 'header.php'; ///Incluição do header
 				    <div class="card p-0">
 					    <img src="img/imagem5.jpg" class="card-img-top" alt="...">
 						    <div class="card-body">
-						        <h5 class="card-title">Desempenho</h5>
-						        <p class="card-text">Nesta funcionalidade você vai encontrar sua evolução nos conteúdos e o seu progresso com gráficos e observaçoes.</p>
+						        <h5 class="card-title">Meu planejamento</h5>
+						        <p class="card-text">Nesta funcionalidade você vai poder criar o seu planejamento de acordo com sua disponibilidade de horários e dias.</p>
 						    </div>
 				    </div>
 				    </div>
